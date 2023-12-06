50b9eebfb65e9c512fe1e39ff040bb92abfaf009
Jose Diaz-Gonzalez <jose@porter.run>
2023-12-06 11:49:47 -0500

    fix: use internal redis client for ping

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
-from django.core.cache import cache
 from django.http import HttpResponse
+from django_redis import get_redis_connection
 
 
 def index(request):
-    return HttpResponse(f"Hello, world. You're at the polls index: {cache.ping()}")
+    conn = get_redis_connection("default")
+    return HttpResponse(f"Hello, world. You're at the polls index: {conn.ping()}")
